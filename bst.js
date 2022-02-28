f3f30a9af454e775d0bb7b72ca491de3b56ba1dc
crose1121 <coreycoding1121@outlook.com>
2022-02-28 10:52:40 -0500

    added contains method

--- bst.js
+++ bst.js
@@ -28,6 +28,26 @@ class Node{
             else{this.right = new Node(valueInput);}
         }
     }
+
+    contains(valueInput) {
+        //check from the BST if root is null first. 
+        // if we're in this method, bst1.root != null
+        if (this.value === valueInput) {
+            return true;
+        }
+        if (valueInput<this.value) {
+            if (this.left) {
+                return this.left.contains(valueInput);
+            }
+            return false;
+        } 
+        if (valueInput>this.value) {
+            if (this.right) {
+                return this.right.contains(valueInput);
+            }
+            return false;
+        }
+    }
 }
 
 class BST{
@@ -37,6 +57,7 @@ class BST{
 
     insert(value) {this.root?this.root.insert(value):this.root = new Node(value);}
 
+    contains(value) {return this.root?this.root.contains(value):false}
 
     display(currentNode=this.root) {
         if (currentNode!=null) {
@@ -58,19 +79,21 @@ class BST{
     }
 }
 
+// console.log(10>null);
+
 let bst = new BST();
 bst.insert(2);
 bst.insert(3);
 bst.insert(5);
-
 bst.insert(1);
-
 bst.insert(7);
-
-bst.insert(4);
+bst.insert(14);
 
 bst.insert(78);
 
 
 console.log(bst.findMax());
 bst.display();
+
+console.log(bst.contains(14)) //true
+console.log(bst.contains(88)) //false
